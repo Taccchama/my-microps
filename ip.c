784c0a894279dabe5db0bd801bef05c14dd48789
YAMAMOTO Masaya <pandax381@gmail.com>
2024-05-26 06:20:08 +0900

    Skeleton of step5

--- ip.c
+++ ip.c
@@ -3,6 +3,8 @@
 #include <stddef.h>
 #include <stdlib.h>
 
+#include "platform.h"
+
 #include "util.h"
 #include "net.h"
 #include "ip.h"
@@ -15,6 +17,12 @@
 
 const ip_addr_t IP_ADDR_ANY       = 0x00000000; /* 0.0.0.0 */
 const ip_addr_t IP_ADDR_BROADCAST = 0xffffffff; /* 255.255.255.255 */
+
+/*
+ * NOTE: if you want to add/delete the entries after net_run(),
+ *       you need to protect these lists with a mutex.
+ */
+static struct ip_iface *ifaces;
 
 int
 ip_addr_pton(const char *p, ip_addr_t *n)
@@ -52,6 +60,27 @@ char *ip_addr_ntop(ip_addr_t n, char *p, size_t size){
 
 // IPパケットの詳細を出力する
 static void ip_print(const uint8_t *data, size_t len) {
+struct ip_iface *
+ip_iface_alloc(const char *unicast, const char *netmask)
+{
+}
+
+/*
+ * NOTE: must not be call after net_run()
+ */
+int
+ip_iface_register(struct net_device *dev, struct ip_iface *iface)
+{
+}
+
+struct ip_iface *
+ip_iface_select(ip_addr_t addr)
+{
+}
+
+static void
+ip_print(const uint8_t *data, size_t len)
+{
     struct ip_hdr *hdr;
     uint8_t v, hl, hlen;
     uint16_t total, offset;
@@ -72,7 +101,7 @@ static void ip_print(const uint8_t *data, size_t len) {
 
     fprintf(stderr, "     total: %u (payload: %u)\n", total, total - hlen);
     fprintf(stderr, "        id: %u\n", ntoh16(hdr->id));
-    
+
     offset = ntoh16(hdr->offset);
 
     fprintf(stderr, "    offset: 0x%04x [flags=%x, offset=%u]\n", offset, offset >> 13, offset & IP_HDR_OFFSET_MASK);
