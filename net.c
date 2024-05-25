963a0d1c63d8bbf6cc97d58435b40164afe39414
YAMAMOTO Masaya <pandax381@gmail.com>
2024-05-26 05:47:47 +0900

    Skeleton of step3

--- net.c
+++ net.c
@@ -7,11 +7,18 @@
 #include "util.h"
 #include "net.h"
 
+struct net_protocol {
+    struct net_protocol *next;
+    uint16_t type;
+    net_protocol_handler_t handler;
+};
+
 /*
  * NOTE: if you want to add/delete the entries after net_run(),
  *       you need to protect these lists with a lock.
  */
 static struct net_device *devices;
+static struct net_protocol *protocols;
 
 struct net_device *net_device_alloc(void) {
     struct net_device *dev;
@@ -113,6 +120,22 @@ int net_device_output(struct net_device *dev, uint16_t type, const uint8_t *data
     }
 
     return 0;
+}
+
+/*
+ * NOTE: must not be call after net_run()
+ */
+int
+net_protocol_register(uint16_t type, net_protocol_handler_t handler)
+{
+}
+
+/*
+ * NOTE: must not be call after net_run()
+ */
+int
+net_protocol_register(uint16_t type, net_protocol_handler_t handler)
+{
 }
 
 // ネットワークデバイスへの入力処理：受信
