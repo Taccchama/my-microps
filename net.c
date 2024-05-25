1a985db49b2db043b9bc710a8bce15850b6f88fa
YAMAMOTO Masaya <pandax381@gmail.com>
2024-05-26 05:31:47 +0900

    Skeleton of step2

--- net.c
+++ net.c
@@ -87,6 +87,16 @@ int net_device_output(struct net_device *dev, uint16_t type, const uint8_t *data
     return 0;
 }
 
+int
+net_input(uint16_t type, const uint8_t *data, size_t len, struct net_device *dev)
+{
+}
+
+int
+net_input(uint16_t type, const uint8_t *data, size_t len, struct net_device *dev)
+{
+}
+
 int net_init(void) {
     infof("initiallize...");
     if (platform_init() == -1) {
